475502db29e3a0fa05e80b720b7f464d261ed38b
jason.regina <jason.regina@noaa.gov>
2026-04-27 13:23:37 +0000

    note data sources

---
+++ storm_events.py
@@ -0,0 +1,11 @@
+"""Storm Events Database.
+
+Source:
+https://www.ncei.noaa.gov/pub/data/swdi/stormevents/csvfiles/
+
+Fields:
+https://www.weather.gov/media/directives/010_pdfs/pd01016005curr.pdf
+
+LSRs:
+https://mesonet.agron.iastate.edu/cgi-bin/request/gis/lsr.py?help
+"""
