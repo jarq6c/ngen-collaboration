e263754c7152cd33a2bfe6a489a582e6a1c5fc00
jason.regina <jason.regina@noaa.gov>
2026-04-28 13:00:59 +0000

    generate parquet store of storms and basins

--- basin_cyclones.py
+++ basin_cyclones.py
@@ -2,10 +2,11 @@
 https://www.ncei.noaa.gov/products/international-best-track-archive
 """
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Optional
 import xarray as xr
 import pandas as pd
 import geopandas as gpd
+from shapely import Polygon, MultiPolygon
 
 GLOBAL_CRS: Literal["ESRI:102010"] = "ESRI:102010"
 """Distance preserving CRS string."""
@@ -14,13 +15,17 @@ def list_unique(s: pd.Series) -> str:
     """Returns all values as a comma-delimited list."""
     return ",".join(s.unique())
 
-def main(
-        cyclone_tracks: Path = Path("IBTrACS.ALL.v04r01.nc"),
-        boundary_source: Path = Path("rfc_nwm_domain_boundaries_4326.geojson"),
-        output_file: Path = Path("rfc_tropical_storms.md")
-) -> None:
-    """Main."""
-    ds = xr.open_dataset(cyclone_tracks)
+def load_IBTrACS(
+        ifile: Path,
+        ofile: Path,
+        mask: Optional[Polygon | MultiPolygon] = None
+) -> gpd.GeoDataFrame:
+    """Load, process, and return GeoDataFrame from IBTrACS NetCDF."""
+    # Check for preprocessed file
+    if ofile.exists():
+        return gpd.read_file(ofile, mask=mask)
+
+    ds = xr.open_dataset(ifile)
 
     # Extract distance to land, nature, and categories
     variables = ["dist2land", "name", "nature", "usa_sshs"]
@@ -28,7 +33,6 @@ def main(
 
     # Determine life-time maximum intensities
     lmi = df[["usa_sshs"]].dropna().reset_index().groupby("storm").max()
-    lmi["usa_sshs"] = lmi["usa_sshs"].apply("{:.0f}".format)
 
     # Drop NaN
     df = df[~df["dist2land"].isna()]
@@ -54,43 +58,67 @@ def main(
     # Project to distance preserving CRS
     gdf = gpd.GeoDataFrame(df.reset_index()).to_crs(GLOBAL_CRS)
 
-    # Load RFC boundaries
-    boundaries = gpd.read_file(boundary_source).to_crs(GLOBAL_CRS)
+    # Add LMI
+    gdf["lmi"] = gdf["storm"].map(lmi["usa_sshs"])
 
-    # Handle duplicate VI entries
-    boundaries.iloc[14, 1] = "VI1"
-    boundaries.iloc[16, 1] = "VI2"
-    boundaries.iloc[17, 1] = "VI3"
+    # Save
+    gdf.to_file(ofile, driver="GPKG")
+    return gpd.read_file(ofile, mask=mask)
 
-    columns: list[str]  = []
-    for row in boundaries.itertuples():
-        # Column
-        col = f"{row.rfc}_{row.domain}"
-        columns.append(col)
-        distances = gdf.geometry.distance(row.geometry)
-        gdf[col] = distances
+def main(
+        cyclone_tracks: Path = Path("IBTrACS.ALL.v04r01.nc"),
+        boundary_source: Path = Path("nwm_usgs_assimilation_gages_basin_boundaries_epsg_4269.gpkg"),
+        output_file: Path = Path("usgs_basin_tropical_storms.parquet"),
+        cyclone_gpkg: Path = Path("data") / "IBTrACS.ALL.v04r01.gpkg"
+) -> None:
+    """Main."""
+    # Pre-process cyclone tracks
+    load_IBTrACS(
+        cyclone_tracks,
+        cyclone_gpkg
+    )
 
-    # Generate tables
-    includes = ["storm", "name", "time", "nature", "usa_sshs"]
-    output = "# Tropical storms by RFC\n\n"
-    for col in columns:
-        # Retain locations within 400,000 m (400 km)
-        data = gdf.loc[gdf[col] < 400_000.0, includes].groupby("storm").agg(
-            Name=pd.NamedAgg(column="name", aggfunc="first"),
-            Start=pd.NamedAgg(column="time", aggfunc="min"),
-            End=pd.NamedAgg(column="time", aggfunc="max"),
-            Designations=pd.NamedAgg(column="nature", aggfunc=list_unique),
-            Categories=pd.NamedAgg(column="usa_sshs", aggfunc=list_unique)
-        ).reset_index().rename(columns={"storm": "Storm ID"})
-        data["Start"] = data["Start"].dt.strftime("%Y-%m-%d %H:%M")
-        data["End"] = data["End"].dt.strftime("%Y-%m-%d %H:%M")
-        data["Lifetime Maximum Intensity"] = data["Storm ID"].map(lmi["usa_sshs"])
+    # Load basin boundaries
+    boundaries: gpd.GeoDataFrame = gpd.read_file(boundary_source).to_crs(GLOBAL_CRS)
+    number_of_basins = boundaries["geometry"].count()
 
-        output += f"## {col}\n\n"
-        output += data.to_markdown() + "\n\n"
+    # Find tracks within 400 km of boundaries
+    dfs: list[pd.DataFrame] = []
+    for _, provider_id, catchment in boundaries.itertuples():
+        # Track progress
+        print(provider_id, f"{len(dfs)} / {number_of_basins}")
 
-    with output_file.open(mode="w", encoding="utf-8") as fo:
-        fo.write(output)
+        # Retrieve locations within 400,000 m (400 km)
+        catchment_points = load_IBTrACS(
+            cyclone_tracks,
+            cyclone_gpkg,
+            mask=catchment.buffer(400_000.0)
+        )
+
+        # Check for empty
+        if catchment_points.empty:
+            continue
+
+        # Aggregate storms
+        catchment_storms = catchment_points.groupby("storm").agg(
+            name=pd.NamedAgg(column="name", aggfunc="first"),
+            impact_start=pd.NamedAgg(column="time", aggfunc="min"),
+            impact_end=pd.NamedAgg(column="time", aggfunc="max"),
+            impact_designations=pd.NamedAgg(column="nature", aggfunc=list_unique),
+            impact_intensity=pd.NamedAgg(column="usa_sshs", aggfunc=list_unique),
+            lifetime_max_intensity=pd.NamedAgg(column="lmi", aggfunc="first")
+        ).reset_index().rename(columns={"storm": "storm_id"})
+
+        # Add site code
+        catchment_storms["usgs_site_code"] = provider_id
+
+        # Add to list
+        dfs.append(catchment_storms)
+
+    # Concat
+    data = pd.concat(dfs, ignore_index=True)
+    print(data)
+    data.to_parquet(output_file)
 
 if __name__ == "__main__":
     main()
